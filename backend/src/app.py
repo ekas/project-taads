ac1289db54f25ef28131d7304de08eaa392bdbc7
ekas <ekaspreet93.singh@gmail.com>
2021-05-24 21:28:52 +0200

    intro to API

---
+++ backend/src/app.py
@@ -0,0 +1,22 @@
+import ingredient_parser
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def root():
+#     return parse_ingredient("12 ounces lean ground beef, preferably 85 percent lean")
+
+# result = ingredient_parser.parse_ingredient("12 ounces lean ground beef, preferably 85 percent lean")
+result = ingredient_parser.parse_ingredient("2x400g cans chopped tomatoes")
+# result = ingredient_parser.parse_ingredient("2 rashers smoked streaky bacon")
+# result = ingredient_parser.parse_ingredient("1 celery stick, finely chopped")
+# result = ingredient_parser.parse_ingredient("500g beef mince")
+# result = ingredient_parser.parse_ingredient("1 1/3 fresh egg")
+# result = ingredient_parser.parse_ingredient("1-2 fresh egg")
+print(f"Found results: \n {result}")
